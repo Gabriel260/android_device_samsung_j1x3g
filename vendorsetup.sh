00f0ef1392631be45f7c27bebc6f366be79a3a98
Gabriel260 <adeilmazk@gmail.com>
2021-01-06 12:43:29 -0300

    j1x3g: fix blue color

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,9 @@
 add_lunch_combo omni_j1x3g-userdebug
+
+if [ -d bootable/recovery/gui/theme/portrait_mdpi ]; then
+rm -rf bootable/recovery/gui/theme/portrait_mdpi/images/*
+cp -rf device/samsung/j1x3g/images/* bootable/recovery/gui/theme/portrait_mdpi/images
+else
+echo "can't find recovery theme!"
+pwd
+fi;
