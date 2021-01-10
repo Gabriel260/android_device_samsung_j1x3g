ef2711c1da6cca672c2cc84741dd2b370208cc70
Gabriel <adeilmazk@gmail.com>
2021-01-10 13:13:19 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,6 @@ LOCAL_PATH := device/samsung/j1x3g
 # Vendorname
 BOARD_VENDOR := samsung
 
-
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -12,9 +11,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a7
 
-
 # Platform
-
 TARGET_BOARD_PLATFORM := sc8830
 #TARGET_BOARD_PLATFORM_GPU := mali-t760mp8
 
@@ -23,40 +20,31 @@ TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 
 
-
-
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := SC7730SE
 
-
-#Kernel
+# Kernel
 BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j1x3g/dt.img
-
-
 # TARGET_KERNEL_SOURCE := kernel/samsung/j1x3g
+TARGET_PREBUILT_KERNEL := device/samsung/j1x3g/kernel
 
-#Partitions 0x360000000
+# Partitions 0x360000000
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x360000000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x360000000
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-TARGET_PREBUILT_KERNEL := device/samsung/j1x3g/kernel
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Recovery
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 TARGET_RECOVERY_INITRC := device/samsung/j1x3g/init.rc
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/twrp.fstab
 
-
-BOARD_HAS_LARGE_FILESYSTEM := true
-TARGET_USERIMAGES_USE_EXT4 := true
-
-BOARD_SUPPRESS_SECURE_ERASE := true
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
