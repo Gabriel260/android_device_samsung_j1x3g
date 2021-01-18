4d97ccf9ddc32a869b95a006212c96bd79e1e843
Gabriel260 <adeilmazk@gmail.com>
2021-01-18 12:26:01 -0300

    j1x3g: edit files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,38 +1,21 @@
-# Define Local path
-LOCAL_PATH := device/samsung/j1x3g
-
-# Vendorname
-BOARD_VENDOR := samsung
-
-# Architecture
+# Platform
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_BOARD_PLATFORM := sc8830
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
-
-# Platform
-TARGET_BOARD_PLATFORM := sc8830
-#TARGET_BOARD_PLATFORM_GPU := mali-t760mp8
-
-# Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 
-
-# Bootloader
-TARGET_NO_BOOTLOADER := true
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_BOOTLOADER_BOARD_NAME := SC7730SE
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
+BOARD_VENDOR := samsung
 
-# Kernel
-BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j1x3g/dt.img
-# TARGET_KERNEL_SOURCE := kernel/samsung/j1x3g
-TARGET_PREBUILT_KERNEL := device/samsung/j1x3g/kernel
+# Config u-boot
+TARGET_NO_BOOTLOADER := true
 
-# Partitions 0x360000000
+# Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x360000000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x360000000
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -40,32 +23,27 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j1x3g/dt.img
+TARGET_PREBUILT_KERNEL := device/samsung/j1x3g/kernel
+
 # Recovery
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
+TARGET_RECOVERY_FSTAB := device/samsung/j1x3g/twrp.fstab
 TARGET_RECOVERY_INITRC := device/samsung/j1x3g/init.rc
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/twrp.fstab
-
-# Include path
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
-
-# TWRP specific build flags
-TW_THEME := portrait_mdpi
-DEVICE_RESOLUTION := 480x800
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
-TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_INTERNAL_STORAGE_PATH := "/internal_sd"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "internal_sd"
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-#TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TW_MAX_BRIGHTNESS := 255
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_THEME := portrait_mdpi
+RECOVERY_SDCARD_ON_DATA := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
-TW_EXCLUDE_SUPERSU := true
-#TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_10x18.h\"
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_MTP_DEVICE := /dev/mtp_usb
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/20200000.usb/gadget/lun%d/file"
-TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
-
