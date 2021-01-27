237fc69a4a1ee7c214876c3a036a13a539bd3b07
Gabriel260 <adeilmazk@gmail.com>
2021-01-27 07:08:41 -0300

    j1x3g: adjust

--- omni_j1x3g.mk
+++ omni_j1x3g.mk
@@ -2,16 +2,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := grandprimeve3g
+PRODUCT_RELEASE_NAME := j1x3g
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := grandprimeve3g
-PRODUCT_NAME := omni_grandprimeve3g
+PRODUCT_DEVICE := j1x3g
+PRODUCT_NAME := omni_j1x3g
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-G531H
+PRODUCT_MODEL := SM-J120H
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
